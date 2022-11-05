7129cd5708e812b5a5a98bfeb5dcbc1da1932649
rdahltorp <109421585+rdahltorp@users.noreply.github.com>
2022-11-05 08:35:05 -0600

    Added frame work for all functions and if statement for the flow of app.

--- script.js
+++ script.js
@@ -3,20 +3,40 @@ const { response } = require('express');
 const inquirer = require('inquirer');
 
 //Additional Functions
+function viewDepts() {
+    //Add a cTable takes the query to view departments
+    //Add mainMenu() call 
+}
+
+function viewRoles() {
+    //Add a cTable takes the query to view roles
+    //Add mainMenu() call 
+
+}
+
+function viewEmps() {
+    //Add a cTable takes the query to view roles
+    //Add mainMenu() call
+}
+
 function addDepartment () {
-    //Add inquirer prompt here for these specific questions. Also need to POST to db. ALso return cTable    
+    //Add inquirer prompt here for these specific questions. Also need to POST to db. ALso return cTable
+    //Add mainMenu() call    
 }
 
 function addRole () {
-    //Add inquirer prompt here for these specific questions. Also need to POST to db. ALso return cTable    
+    //Add inquirer prompt here for these specific questions. Also need to POST to db. ALso return cTable
+    //Add mainMenu() call    
 }
 
 function addEmployee () {
-    //Add inquirer prompt here for these specific questions. Also need to POST to db. ALso return cTable    
+    //Add inquirer prompt here for these specific questions. Also need to POST to db. ALso return cTable
+    //Add mainMenu() call    
 }
 
 function updateEmployee () {
-    //Add inquirer prompt here for these specific questions. Also need to PUT to db. ALso return cTable    
+    //Add inquirer prompt here for these specific questions. Also need to PUT to db. 
+    //Add mainMenu() call   
 }
 
 function mainMenu() {
@@ -33,6 +53,22 @@ function mainMenu() {
         } else {
             console.log(`You have selected ${response.menu}`)
             mainMenu()
+        }
+
+        if (answer.menu === "View all departments") {
+            viewDepts()
+        } else if (answer.menu === "View all roles") {
+            viewRoles()
+        } else if (answer.menu === "View all employees") {
+            viewEmps()
+        } else if (answer.menu === "Add a department") {
+            addDepartment()
+        } else if (answer.menu === "Add a role") {
+            addRole()
+        } else if (answer.menu === "Add a employee") {
+            addEmployee()
+        } else if (answer.menu === "Update an employee role") {
+            updateEmployee()
         }
     });
 
