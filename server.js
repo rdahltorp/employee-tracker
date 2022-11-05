68b598a8fe4d6aec5bb50c15947dc987e3e0821c
rdahltorp <109421585+rdahltorp@users.noreply.github.com>
2022-11-05 10:52:04 -0600

    Added WIP framework code to the menu functions in server.js.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cTable = require('console.table');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Server } = require('http');
+const { type } = require('os');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -24,48 +25,137 @@ console.log("You are now connected to the employees_db database")
 
 //Additional Functions
 function viewDepts() {
-    //Add a cTable takes the query to view departments
     const sql = fs.readFileSync('./queries/viewDepts_query.sql').toString();
 
-    db.query(sql, (err, rows) => {
+    db.query(sql, async (err, rows) => {
         if(err) {
             console.log(err);
         } else {
             console.table(rows);
+            mainMenu()
         }
-    })
-    //Add mainMenu() call 
-    mainMenu()
-}
+    });
+};
 
 function viewRoles() {
-    //Add a cTable takes the query to view roles
-    //Add mainMenu() call 
+    const sql = fs.readFileSync('./queries/viewRoles_query.sql').toString();
 
-}
+    db.query(sql, async (err, rows) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.table(rows);
+            mainMenu()
+        }
+    });
+};
 
 function viewEmps() {
-    //Add a cTable takes the query to view roles
-    //Add mainMenu() call
+    //RECIVING ERROR
+    const sql = fs.readFileSync('./queries/viewEmps_query.sql').toString();
+
+    db.query(sql, async (err, rows) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.table(rows);
+            mainMenu()
+        }
+    });
 }
 
 function addDepartment () {
     //Add inquirer prompt here for these specific questions. Also need to POST to db. ALso return cTable
+    inquirer
+        .prompt([{
+            message: "What is the name of the new department?",
+            type: 'input',
+            name: 'name'
+        }])
+        .then(answer => (console.log(answer)));
     //Add mainMenu() call    
 }
 
 function addRole () {
     //Add inquirer prompt here for these specific questions. Also need to POST to db. ALso return cTable
+    inquirer
+        .prompt([
+            {
+                message: "What is the name of the role?",
+                type: 'input',
+                name: 'title'
+            },
+            {
+                message: 'What is the salary for this role?',
+                type: 'input',
+                name: 'salary'
+            },
+            {
+                message: 'What department number is this role in?',
+                type: 'input',
+                name: 'department_id'
+            }
+        ])
+        .then(answer => (console.log(answer)));
     //Add mainMenu() call    
 }
 
 function addEmployee () {
     //Add inquirer prompt here for these specific questions. Also need to POST to db. ALso return cTable
+    inquirer
+        .prompt([
+            {
+                message: "What is the new employee's first name?",
+                type: 'input',
+                name: 'first_name'
+            },
+            {
+                message: "What is the new employee's last name?",
+                type: 'input',
+                name: 'last_name'
+            },
+            {
+                message: "What is this employee's role id number?",
+                type: 'input',
+                name: 'role_id'
+            },
+            {
+                message: "What is this employee's manager id number?",
+                type: 'input',
+                name: 'manager_id'
+            }
+        ])
+        .then(answer => (console.log(answer)));
     //Add mainMenu() call    
 }
 
-function updateEmployee () {
+function updateEmployeeRole () {
     //Add inquirer prompt here for these specific questions. Also need to PUT to db. 
+    inquirer
+        .prompt([
+            {
+                message: "Which employee would you like to update?",
+                type: 'list',
+                name: 'employee',
+                choices: []
+            },
+            {
+                message: "What is the new employee's last name?",
+                type: 'input',
+                name: 'last_name'
+            },
+            {
+                message: "What is this employee's role id number?",
+                type: 'input',
+                name: 'role_id'
+            },
+            {
+                message: "What is this employee's manager id number?",
+                type: 'input',
+                name: 'manager_id'
+            }
+        ])
+        .then(answer => (console.log(answer)));
     //Add mainMenu() call   
 }
 
@@ -78,32 +168,39 @@ function mainMenu() {
         choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Quit"]
     }])
     .then(answer => {
-        // if(answer.menu === "Quit"){
-        //     return
-        // } else {
-        //     console.log(`You have selected ${answer.menu}`)
-        //     mainMenu()
-        // }
+        switch (answer.menu) {
+            case "View all departments":
+                viewDepts()
+                break;
+            
+            case "View all roles":
+                viewRoles()
+                break;
+            
+            case "View all employees":
+                viewEmps()
+                break;
 
-        if (answer.menu === "View all departments") {
-            viewDepts();
-        } else if (answer.menu === "View all roles") {
-            viewRoles();
-        } else if (answer.menu === "View all employees") {
-            viewEmps();
-        } else if (answer.menu === "Add a department") {
-            addDepartment();
-        } else if (answer.menu === "Add a role") {
-            addRole();
-        } else if (answer.menu === "Add a employee") {
-            addEmployee();
-        } else if (answer.menu === "Update an employee role") {
-            updateEmployee();
-        } else if (answer.menu === "Quit") {
-            return;
+            case "Add a department":
+                addDepartment()
+                break;
+            
+            case "Add a role":
+                addRole()
+                break;
+            
+            case "Add an employee":
+                addEmployee()
+                break;
+            
+            case "Update an employee role":
+                updateEmployeeRole()
+                break;
+        
+            default:
+                break;
         }
     });
-
 }
 
 mainMenu()
